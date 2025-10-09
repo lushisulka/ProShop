92340063f50e24de80699eb248faf413c416a315
lushisulka <sulkaengjellushe5@gmail.com>
2025-10-09 15:10:15 +0200

    Satrting MongoDB & Modeling some data like order, user and product

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../products';
+import axios from 'axios';
+
 
 const HomeScreen = () => {
+    const [products, setproducts] = useState([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const { data } = await axios.get('/api/products');
+            setproducts(data);
+        };
+        fetchProducts();
+    }, []);
+
     return (
         <>
             <h1>Latest Products</h1>
